f5148f4dd31bf5dc54b48bd5fd0de9fb5a47656f
Pascal Keusch <pascal.keusch@stud.hslu.ch>
2019-04-25 21:24:44 +0200

    Remove App

--- app/src/main/java/ch/mobpro/eventapp/repository/SessionTokenRepository.java
+++ app/src/main/java/ch/mobpro/eventapp/repository/SessionTokenRepository.java
@@ -1,26 +1,14 @@
 package ch.mobpro.eventapp.repository;
 
-import android.app.Application;
-import ch.mobpro.eventapp.App;
-import ch.mobpro.eventapp.di.component.ApplicationComponent;
-import ch.mobpro.eventapp.di.component.DaggerApplicationComponent;
-import ch.mobpro.eventapp.dto.JwtTokenResponse;
 import ch.mobpro.eventapp.dto.UserCredentials;
 import ch.mobpro.eventapp.dto.UserRegistrationForm;
-import ch.mobpro.eventapp.model.User;
 import ch.mobpro.eventapp.service.AuthInterceptor;
 import ch.mobpro.eventapp.service.AuthService;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import io.reactivex.Observable;
 import io.reactivex.Single;
-import okhttp3.Interceptor;
-import okhttp3.Request;
-import okhttp3.Response;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.io.IOException;
 
 @Singleton
 public class SessionTokenRepository{
--- app/src/main/java/ch/mobpro/eventapp/service/RestClientModule.java
+++ app/src/main/java/ch/mobpro/eventapp/service/RestClientModule.java
@@ -1,18 +1,12 @@
 package ch.mobpro.eventapp.service;
 
-import ch.mobpro.eventapp.App;
-import ch.mobpro.eventapp.di.module.ViewModelModule;
-import ch.mobpro.eventapp.repository.SessionTokenRepository;
 import dagger.Module;
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
-
-import javax.inject.Inject;
 
 @Module
 public class RestClientModule {
