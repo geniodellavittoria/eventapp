2c231e6ec8a481905d0bceaabf2a6b4c08086838
Tim Bolzern <t.bolzern@gmail.com>
2019-04-28 21:59:46 +0200

    added maps to createEvent

--- app/src/main/java/ch/mobpro/eventapp/activity/CreateEventActivity.java
+++ app/src/main/java/ch/mobpro/eventapp/activity/CreateEventActivity.java
@@ -7,6 +7,7 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.Toolbar;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,13 +16,19 @@ import ch.mobpro.eventapp.base.BaseActivity;
 import ch.mobpro.eventapp.databinding.ActivityCreateEventBinding;
 import ch.mobpro.eventapp.service.EventCategoryService;
 import ch.mobpro.eventapp.viewmodel.CreateEventViewModel;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import javax.inject.Inject;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Calendar;
 
-public class CreateEventActivity extends BaseActivity<ActivityCreateEventBinding> {
+public class CreateEventActivity extends BaseActivity<ActivityCreateEventBinding> implements OnMapReadyCallback {
 
     private String TAG = this.getClass().getSimpleName();
 
@@ -35,11 +42,8 @@ public class CreateEventActivity extends BaseActivity<ActivityCreateEventBinding
     private EditText pickEndDate;
     private TimePickerDialog timePickerDialog;
     private DatePickerDialog datePickerDialog;
-    private final EventCategoryService categoryService;
-
-    public CreateEventActivity(EventCategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
+    private GoogleMap mMap;
+    private EventLocationMapsFragmentActivity eventLocationMapsFragmentActivity;
 
     @Override
     protected int layoutRes() {
@@ -106,6 +110,10 @@ public class CreateEventActivity extends BaseActivity<ActivityCreateEventBinding
             }, year, month, day);
             datePickerDialog.show();
         });
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     private void onCreateSuccess(boolean isSuccess) {
@@ -115,5 +123,14 @@ public class CreateEventActivity extends BaseActivity<ActivityCreateEventBinding
             Intent intent = new Intent(this, EventListActivity.class);
             startActivity(intent);
         }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng rotkreuz = new LatLng(47.1358, 8.4667);
+        mMap.addMarker(new MarkerOptions().position(rotkreuz).title("Marker in Rotkreuz"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(rotkreuz));
     }
 }
