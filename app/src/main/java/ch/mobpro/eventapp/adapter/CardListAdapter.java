532f2d9edeb58d678529c4fa436dea889c5e8a64
Pascal Keusch <pascal.keusch@stud.hslu.ch>
2019-04-29 19:46:08 +0200

    Style cards

--- app/src/main/java/ch/mobpro/eventapp/adapter/CardListAdapter.java
+++ app/src/main/java/ch/mobpro/eventapp/adapter/CardListAdapter.java
@@ -1,6 +1,5 @@
 package ch.mobpro.eventapp.adapter;
 
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,10 +9,22 @@ import android.widget.TextView;
 import ch.mobpro.eventapp.R;
 import ch.mobpro.eventapp.model.Event;
 import org.jetbrains.annotations.NotNull;
+import org.w3c.dom.Text;
 
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.List;
+import java.util.Locale;
 
 public class CardListAdapter extends RecyclerView.Adapter<CardListAdapter.EventCardViewHolder> {
+
+    DateTimeFormatter formatter =
+            DateTimeFormatter.ofLocalizedDateTime( FormatStyle.SHORT )
+                    .withLocale( Locale.UK )
+                    .withZone( ZoneId.systemDefault() );
+
+
     private List<Event> events;
 
     // Provide a reference to the views for each data item
@@ -21,12 +32,23 @@ public class CardListAdapter extends RecyclerView.Adapter<CardListAdapter.EventC
     // you provide access to all the views for a data item in a view holder
     public static class EventCardViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public ImageView imageView;
+        public ImageView eventImage;
         public TextView eventNameText;
+        public TextView eventCategoryText;
+        public TextView eventAvailablePlaceText;
+        public TextView eventPriceText;
+        public TextView eventDateText;
+        public TextView eventLocationText;
+
         public EventCardViewHolder(View view) {
             super(view);
-            imageView = view.findViewById(R.id.event_image);
+            eventImage = view.findViewById(R.id.event_image);
+            eventCategoryText = view.findViewById(R.id.event_category_text);
             eventNameText = view.findViewById(R.id.event_name_text);
+            eventAvailablePlaceText = view.findViewById(R.id.event_available_place_text);
+            eventPriceText = view.findViewById(R.id.event_price_text);
+            eventDateText = view.findViewById(R.id.event_date_text);
+            eventLocationText = view.findViewById(R.id.event_location_text);
         }
     }
 
@@ -53,8 +75,14 @@ public class CardListAdapter extends RecyclerView.Adapter<CardListAdapter.EventC
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
         holder.eventNameText.setText(events.get(position).getName());
-        //holder.imageView.setIma
-
+        if (!events.get(position).getCategories().isEmpty()) {
+            holder.eventCategoryText.setText(events.get(position).getCategories().get(0).getCategory());
+        }
+        holder.eventPriceText.setText(String.format("Eintritt: CHF %1$,.2f", events.get(position).getPrice()));
+        holder.eventAvailablePlaceText.setText("/" + events.get(position).getPlace().toString());
+        String eventDate = formatter.format(events.get(position).getStartTime()) + " - " +
+                formatter.format(events.get(position).getEndTime());
+        holder.eventDateText.setText(eventDate);
     }
 
     // Return the size of your dataset (invoked by the layout manager)
