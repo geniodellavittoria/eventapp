86c4d72fde485d9bc6ee3fe0d764e7053bbd1eea
Tim Bolzern <t.bolzern@gmail.com>
2019-05-05 13:56:40 +0200

    added search button to appbar

--- app/src/main/java/ch/mobpro/eventapp/activity/DetailEventActivity.java
+++ app/src/main/java/ch/mobpro/eventapp/activity/DetailEventActivity.java
@@ -14,6 +14,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.*;
 import ch.mobpro.eventapp.R;
@@ -33,7 +34,10 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import javax.inject.Inject;
-import java.time.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
 
 public class DetailEventActivity extends BaseActivity<ActivityDetailEventBinding> implements OnMapReadyCallback {
 
